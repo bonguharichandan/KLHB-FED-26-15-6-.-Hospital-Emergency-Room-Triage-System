3a1d6dca1adc3a98376988ca7667591596561069
hari <bonguharichandan@gmail.com>
2026-09-22 15:18:07 +0530

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 class HospitalTriageSystem{
   public static void main(String [] args){
-    
+     Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+        sc.close();
   }
 }
